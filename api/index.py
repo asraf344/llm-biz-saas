acffde941ac930452311474163ead4a26e988739
Md. Asraful Haque <asraf344@gmail.com>
2026-06-16 15:58:20 +0600

    add streaming

--- api/index.py
+++ api/index.py
@@ -1,17 +1,27 @@
 from fastapi import FastAPI  # type: ignore
-from fastapi.responses import PlainTextResponse  # type: ignore
+from fastapi.responses import StreamingResponse  # type: ignore
+
 from google import genai  # type: ignore
 
 app = FastAPI()
 
 client = genai.Client()
 
-@app.get("/api", response_class=PlainTextResponse)
+@app.get("/api")
 def idea():
     prompt = "Come up with a new business idea for AI Agents"
-    response = client.models.generate_content(
+    stream = client.models.generate_content_stream(
         model="gemini-2.5-flash",
         contents=prompt
     )
 
-    return response.text
+    def event_stream():
+        for chunk in stream:
+            text = chunk.text
+            if text:
+                lines = text.split("\n")
+                for line in lines:
+                    yield f"data: {line}\n"
+                yield "\n"
+
+    return StreamingResponse(event_stream(), media_type="text/event-stream")
